b5251869a2932cc3d9b442a6c50fe29feeb7207d
TO <th.theophyl.hoguet@gmail.com>
2023-09-20 15:53:12 +0200

    Part III.1

--- code/machine/console.h
+++ code/machine/console.h
@@ -23,9 +23,9 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#include <stdio.h>
 #include "copyright.h"
 #include "utility.h"
-#include <stdio.h>
 
 // The following class defines a hardware console device.
 // Input and output to the device is simulated by reading
@@ -38,48 +38,47 @@
 // by calling TX() has been "put", so that the next character can be
 // written.
 
-class Console:public dontcopythis {
-  public:
-    Console(const char *readFile, const char *writeFile, VoidFunctionPtr readAvailHandler,
-        VoidFunctionPtr writeDoneHandler, void *callArg);
-                                // initialize the hardware console device,
-                                // registers the readAvailHandler and writeDoneHandler
-                                // callbacks
+class Console : public dontcopythis {
+   public:
+	Console(const char* readFile, const char* writeFile, VoidFunctionPtr readAvailHandler, VoidFunctionPtr writeDoneHandler, void* callArg);
+	// initialize the hardware console device,
+	// registers the readAvailHandler and writeDoneHandler
+	// callbacks
 
-    ~Console();                 // clean up console emulation
+	~Console();  // clean up console emulation
 
-// external interface -- Nachos kernel code can call these
-    void TX(int ch);            // Write "ch" to the console display,
-                                // and return immediately.  "writeDone"
-                                // is called when the I/O completes.
+	// external interface -- Nachos kernel code can call these
+	void TX(int ch);  // Write "ch" to the console display,
+	                  // and return immediately.  "writeDone"
+	                  // is called when the I/O completes.
 
-    int RX(void);               // Poll the console input.  If a char is
-                                // available, return it.  Otherwise, crash.
-                                // EOF is returned if the end of the input
-                                // file was reached.
-                                // "readDone" is called whenever there is
-                                // a char to be gotten
+	int RX(void);  // Poll the console input.  If a char is
+	               // available, return it.  Otherwise, crash.
+	               // EOF is returned if the end of the input
+	               // file was reached.
+	               // "readDone" is called whenever there is
+	               // a char to be gotten
 
-// internal emulation routines -- DO NOT call these.
-    void WriteDone(void);	// internal routines to signal I/O completion
-    void CheckCharAvail(void);
+	// internal emulation routines -- DO NOT call these.
+	void WriteDone(void);  // internal routines to signal I/O completion
+	void CheckCharAvail(void);
 
-  private:
-    int readFileNo;                     // UNIX file emulating the keyboard
-    int writeFileNo;                    // UNIX file emulating the display
-    VoidFunctionPtr writeHandler;       // Interrupt handler to call when
-                                        // the TX I/O completes
-    VoidFunctionPtr readHandler;        // Interrupt handler to call when
-                                        // a character arrives from the keyboard
-    void *handlerArg;                   // argument to be passed to the
-                                        // interrupt handlers
-    bool putBusy;                       // Is a TX operation in progress?
-                                        // If so, you can't do another one!
-    int incoming;                       // Contains the character to be read,
-                                        // if there is one available.
-                                        // Otherwise contains EOF.
-    static int stdin_busy;              // Whether stdin is already read from
-                                        // by a console.
+   private:
+	int readFileNo;                // UNIX file emulating the keyboard
+	int writeFileNo;               // UNIX file emulating the display
+	VoidFunctionPtr writeHandler;  // Interrupt handler to call when
+	                               // the TX I/O completes
+	VoidFunctionPtr readHandler;   // Interrupt handler to call when
+	                               // a character arrives from the keyboard
+	void* handlerArg;              // argument to be passed to the
+	                               // interrupt handlers
+	bool putBusy;                  // Is a TX operation in progress?
+	                               // If so, you can't do another one!
+	int incoming;                  // Contains the character to be read,
+	                               // if there is one available.
+	                               // Otherwise contains EOF.
+	static int stdin_busy;         // Whether stdin is already read from
+	                               // by a console.
 };
 
-#endif // CONSOLE_H
+#endif  // CONSOLE_H
