7dce28fd107d9dcb1177bac7dcdb5eb0b8c52f98
Abdoulaye Diallo <abdoulaye.diallo@etu.u-bordeaux.fr>
2023-09-27 15:15:13 +0200

    update IV.4

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -4,16 +4,17 @@
 void print(char c, int n)
 {
     int i;
-    #if 0
+
     for (i = 0; i < n; i++) {
         PutChar(c + i);
     }
     PutChar('\n');
-    #endif
+
 }
 
 int main()
 {
     print('a',4);
+    return 0;
     Halt();
 }
