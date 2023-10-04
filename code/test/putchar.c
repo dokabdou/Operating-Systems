7466e2dd4e2abf9d142882711f8da6d671555200
TO <th.theophyl.hoguet@gmail.com>
2023-10-04 15:21:41 +0200

    changed tests

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -5,13 +5,23 @@ void print(char c, int n) {
 	int i;
 
 	for (i = 0; i < n; i++) {
-		PutChar(c + i);
+		PutChar(c + (i % 26));
 	}
 	PutChar('\n');
 }
 
-int main() {
-	print('a', 4);
+// soit sans argument et ca print 30 char soit
+// avec un argument et ca print le nombre de char spécifié,
+// il suffit de regarder s'il y a le meme nombre de characteres
+// que spécifié et que c'est bien l'alphabet
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		print('a', 30);
+		Halt();
+		return 0;
+	}
+	int size = atoi(argv[1]);
+	print('a', size);
 	Halt();
 	return 0;
 }
--- code/test/putstring.c
+++ code/test/putstring.c
@@ -1,8 +1,29 @@
 #ifdef CHANGED
 #include "syscall.h"
 
-int main() {
-	PutString("Salut ca va ?\n");
+// TODO: changer c'est pas dans la memeoire
+void fillString(char* toTest, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		toTest[i] = 'a' + i % 26;
+	}
+	toTest[size] = '\0';
+}
+
+// soit sans argument et ca teste avec un string de 10 char soit
+// avec un argument et ca teste avec un string de la taille de l'argument,
+// il suffit de regarder s'il y a le meme nombre de characteres que spécifié
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		char toTest[10];
+		fillString(toTest, 10);
+		PutString(toTest);
+		return 0;
+	}
+	int size = atoi(argv[1]);
+	char string[size];
+	fillString(string, size);
+	PutString(string);
 	Halt();
 	return 0;
 }
