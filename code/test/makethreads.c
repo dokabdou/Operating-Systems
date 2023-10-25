aa003ac148856fd3ba61f70ffa412e91714ce713
TO <th.theophyl.hoguet@gmail.com>
2023-10-25 14:47:23 +0200

    formatting + syscall

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -1,16 +1,15 @@
 #ifdef CHANGED
 #include "syscall.h"
 
-
-int f(a){
-    PutString(a);
-    ThreadExit();
+int f(a) {
+	PutString(a);
+	ThreadExit();
 }
 
-int main(){
-    ThreadCreate(f, "test");
-    PutString("main");
-    ThreadExit();
+int main() {
+	ThreadCreate(f, "test");
+	PutString("main");
+	ThreadExit();
 }
 
 #endif  // CHANGED
